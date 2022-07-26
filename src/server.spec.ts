161f095a9cdd1e99f1f0926433f75ed2f2d96e32
Daniel Borges <danprog91@gmail.com>
2022-07-25 22:45:59 -0300

    Implementando testes

---
+++ src/server.spec.ts
@@ -0,0 +1,30 @@
+import { response } from 'express';
+import request from 'supertest';
+import { app } from './app';
+
+// describe("[GET] /", () => {
+//   it("Teste simples", async () => {
+//     expect(true).toBe(true)
+//   })
+// })
+
+
+describe("Teste simples", ()=> {
+  test("Verifica se o retorno da função é true", async() => {
+    const res = await request(app).get('/');
+
+    expect(res.body).toEqual({alive: true})
+  });
+
+  test("Verifica se o retorno possui status 200", async () => {
+    const res = await request(app).get('/');
+
+    expect(res.statusCode).toEqual(200);
+  })
+
+  test("Verifica se possuímos a propriedade alive", async() => {
+    const res = await request(app).get('/');
+
+    expect(res.body).toHaveProperty('alive')
+  });
+})
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,4 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/', (request, response) => {
-  return response.status(200).json({alive: true});
-});
+import { app } from "./app";
 
 app.listen(3333, () => {
   console.log("Server iniciado na porta 3333!")
